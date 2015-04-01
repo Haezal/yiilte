8bcbf9fc0cd2f7c7b35ce41907fd34f0f691fe13
Haezal Musa <ezalepy@gmail.com>
2015-04-01 16:40:23 +0800

    change site index page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,10 @@
-<?php
-/* @var $this SiteController */
+<div class="site-index ">
 
-$this->pageTitle=Yii::app()->name;
-?>
+    <div class="header vert">
+        <div class="container">
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+            <h1><?php echo Yii::app()->name ?></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+        </div>
+    </div>
+</div>
